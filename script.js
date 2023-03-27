395d5717aa5a87ad04aa94335455f653430a2ed9
prudhvigana <122306094+prudhvigana@users.noreply.github.com>
2023-03-27 20:48:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+
+async function fetchData(callback) {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await response.json();
+  callback(data);
+}
+
+function displayMessage(data) {
+	document.getElementById("output").innerText = data.title;
+}
+
+// fetchData(displayMessage);
+// let sample = fetchData(displayMessage);
+// let btn1 = document.querySelector("#btn");
